ea4486ac0dfa60d5d8516b471795d851256e2a05
AlexisBrgs <alexis.bourgeois@le-campus-numerique.fr>
2020-12-08 12:04:03 +0100

    first class commit

---
+++ src/test/java/com/example/dungeon/DungeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dungeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
